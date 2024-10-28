e9fe3b67ce95df8b2134c2ae0348340cf1fa68f1
aGold <93511034+aGoldDev@users.noreply.github.com>
2024-10-28 22:55:24 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll('section');
+const sidebarLinks = document.querySelectorAll('.sidebar a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 60) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    sidebarLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(current)) {
+            link.classList.add('active');
+        }
+    });
+});
